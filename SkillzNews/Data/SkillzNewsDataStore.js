fcfff7d97c96621b50ce79d240fc3b669cd30eff
Calvin <ck@skillz.com>
2020-01-05 18:25:05 -0800

    Adding fake message to handle API errors

--- SkillzNews/Data/SkillzNewsDataStore.js
+++ SkillzNews/Data/SkillzNewsDataStore.js
@@ -19,12 +19,29 @@ class SkillzNewsController {
       this.skillzMessages = SkillzNewsMessage.make(skillzMesagesData);
       completion(true);
     }).catch((error: *) => {
+      this.skillzMessages = [ new SkillzMessage(this.fakemesageForError()) ];
       completion(true);
     });
   }
 
   getSkillzMessages(): Array<SkillzNewsMessage> {
     return this.skillzMessages;
+  }
+
+  fakemesageForError(): SkillzNewsMessage {
+    const currentDate = new Date();
+    const month = currentDate.getMonth() + 1;
+    const formattedDate = currentDate.getFullYear() + '-' + month + '-' + currentDate.getDate();
+
+    const skillzMessage = [{
+      id: -1,
+      message: "Please refresh and try again later.",
+      title: "Skillz News is currently unavailable",
+      game_specific: false,
+      start_date: formattedDate,
+    }];
+
+    return skillzMessage;
   }
 }
 
